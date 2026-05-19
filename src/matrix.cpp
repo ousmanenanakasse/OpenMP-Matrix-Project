01780720e42fa77e346ba2b3aeb6be473c1a906b
Ousmane Nanakasse <nanakasseo06@gmail.com>
2026-05-19 18:01:32 +0300

    Added OpenMP matrix benchmark and results

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,37 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <omp.h>
+#include <fstream>
 
 using namespace std;
 
-void run_test(int num_threads, int N)
-{
-    vector<vector<int>> A(N, vector<int>(N, 1)), B(N, vector<int>(N, 2)), C(N, vector<int>(N, 0));
-
-    omp_set_num_threads(num_threads);
-
-    double start = omp_get_wtime();
-#pragma omp parallel for
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            for (int k = 0; k < N; k++)
-                C[i][j] += A[i][k] * B[k][j];
-
-    double duration = omp_get_wtime() - start;
-    cout << "Threads: " << num_threads << " | Time: " << duration << " seconds" << endl;
-}
-
 int main()
 {
-    int N = 1000;
-    int tests[] = {1, 2, 4, 8};
+    int N = 150;
+    int max_threads = 1000;
 
-    cout << "--- Starting Matrix Multiplication Benchmarks ---" << endl;
+    cout << "--- Starting Thread Scaling Benchmark (1 to 1000) ---" << endl;
 
-    for (int t : tests)
+    ofstream file("data/results.csv");
+    file << "Threads,Time_Seconds\n";
+
+    for (int num_threads = 1; num_threads <= max_threads; num_threads++)
     {
-        run_test(t, N);
+        omp_set_num_threads(num_threads);
+
+        vector<vector<int>> A(N, vector<int>(N, 1));
+        vector<vector<int>> B(N, vector<int>(N, 2));
+        vector<vector<int>> C(N, vector<int>(N, 0));
+
+        double start = omp_get_wtime();
+
+#pragma omp parallel for
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                for (int k = 0; k < N; k++)
+                    C[i][j] += A[i][k] * B[k][j];
+
+        double duration = omp_get_wtime() - start;
+
+        cout << "Threads: " << num_threads
+             << " | Time: " << duration << " seconds" << endl;
+
+        file << num_threads << "," << duration << "\n";
     }
+
+    file.close();
+
+    cout << "\nDONE! All results are saved in data/results.csv" << endl;
 
     return 0;
 }
